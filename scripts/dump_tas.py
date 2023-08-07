175e8ab4a6fa9bd60ec2a4eb2e275664b10c2398
Peter Bui <pbui@nd.edu>
2023-08-07 15:46:58 -0400

    scripts/dump_tas: fix warning

--- scripts/dump_tas.py
+++ scripts/dump_tas.py
@@ -2,5 +2,5 @@
 
 import yaml
 
-for index, ta in enumerate(yaml.load(open('static/yaml/tas.yaml')), 1):
+for index, ta in enumerate(yaml.safe_load(open('static/yaml/tas.yaml')), 1):
     print(f'{index}. {ta["name"]} ({ta["netid"]}@nd.edu)')
